f54ed102707bdd4e215dd3196ea52a5115e04019
datodato788 <151830320+datodato788@users.noreply.github.com>
2024-08-09 15:21:09 +0400

    +10 =20

---
+++ 11index.js
@@ -0,0 +1,4 @@
+// 11.repeat() ბრუნდება ახალი სტრიქონი, რომელიც შედგება არსებული სტრიქონის გამეორებით მოცემული რაოდენობის.
+const test1 = "dato"
+const test2 = test1.repeat(3)
+console.log(test2)//datodatodato  ანუ სამჯერ გაიემორა რა
